c03fe6dab958cecb7820dfcdc1e28bba107bd7ee
Adarsh <adarshdev85219@gmail.com>
2024-10-29 00:36:24 +0530

    data change

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -130,7 +130,7 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    emailjs.sendForm('service_tox7kqs', 'template_nv7k7mj', form.current, 'SybVGsYS52j2TfLbi')
+    emailjs.sendForm('service_665gs2s', 'template_g1j98xn', form.current, 'suwCGk72mrd3CMEem')
       .then((result) => {
         setOpen(true);
         form.current.reset();
