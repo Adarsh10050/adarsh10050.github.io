73c055250247fb5ca9767f93a2a39bea40f55435
Adarsh <adarshdev85219@gmail.com>
2025-05-31 01:28:31 +0530

    updated portfolio with new changes

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Adarsh10050",
   resume:
-    "https://drive.google.com/file/d/107AmoSTvi66F6zBSX8ruNmGqj5_e6w4h/view?usp=sharing",
+    "https://drive.google.com/file/d/1yThcgN1K0iAn5d1YIPZS4eiV-CJnT3Ju/view",
   github: "https://github.com/Adarsh10050",
   linkedin: "https://www.linkedin.com/in/adarsh-kumar-826936285/",
   twitter: "https://x.com/Adarshdev121p",
@@ -210,7 +210,7 @@ export const experiences = [
     ],
     doc: "",
   },
-  
+
 ];
 
 export const education = [
